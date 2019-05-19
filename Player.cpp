6341ccf738f97b8e8cb438dcb9a379d96429642b
Andy <41456180+s3722315@users.noreply.github.com>
2019-05-19 16:21:09 +1000

    Add files via upload
    updated player to string

---
+++ Player.cpp
@@ -0,0 +1,90 @@
+#include "Player.h"
+Player::Player(std::string name, int score, LinkedList* hand)
+{
+	this->name = name;
+	this->score = score;
+	this->hand = hand;
+}
+
+Player::Player(std::string name) {
+	this->name = name;
+	this->score = 0;
+}
+LinkedList* Player::getHand()
+{
+	return this->hand;
+}
+
+void Player::initalHand(Bag* bag) {
+	const int numsOfTiles = 6;
+	for (int i = 0; i < numsOfTiles; ++i) {
+		Tile* tile = bag->getTile();
+		addToHand(tile);
+	}
+}
+
+void Player::printHand() {
+	this->hand->printAll();
+}
+
+int Player::getScore() {
+	return this->score;
+}
+
+std::string Player::getName() {
+	return this->name;
+}
+
+void Player::setScore(int score) {
+	this->score = score;
+}
+void Player::addScore(int plusValue) {
+	this->score += plusValue;
+}
+
+void Player::addToHand(Tile* addTile) {
+	this->hand->addBack(addTile);
+}
+
+void Player::replaceFromHand(Tile* removeTile, Bag* bag) {
+	if (this->hand->contains(removeTile)) {
+
+		Tile* movedTile = removeTile;
+		this->hand->removeNode(removeTile);
+
+		Tile* addedTile = bag->getTile();
+		addToHand(addedTile);
+
+		bag->addTile(movedTile);
+	}
+}
+
+Tile* Player::placeTile(Tile* placedTile, Bag* bag)
+{
+	Tile* placingTile = nullptr;
+	if (this->hand->contains(placedTile)) {
+
+		placingTile = this->hand->getSpecTile(placedTile);
+		this->hand->removeNode(placedTile);
+
+		Tile* addedTile = bag->getTile();
+		addToHand(addedTile);
+	}
+
+	return placingTile;
+}
+
+std::string Player::toString()
+{
+	std::string returnString = "";
+	if (this->name.empty() == false) {
+		/* code */
+		returnString += this->name;
+		returnString += '\n';
+		returnString += std::to_string(this->score);
+		returnString += '\n';
+
+		returnString += hand->toString();
+	}
+	return returnString;
+}
